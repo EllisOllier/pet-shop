c4da1be25a3905983e2b4d06187862462868ad36
EllisOllier <ellisollier1@gmail.com>
2025-10-31 13:15:55 +0000

    Added stroke to Dog and Cat in main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args){
         Cat cat1 = new Cat("Timmeh", 3, 249.99);
         System.out.println(cat1.makeNoise());
+        System.out.println(cat1.stroke());
+
+        Dog dog1 = new Dog("Zeus", 1, 1250);
+        System.out.println(dog1.makeNoise());
+        System.out.println(dog1.stroke());
+
+        Goldfish fish1 = new Goldfish("Nemo", 12, 75);
+        System.out.println(fish1.makeNoise());
     }
 }

657b43977bbd6e5c76c0be1ac7250c813e031709
EllisOllier <ellisollier1@gmail.com>
2025-10-31 12:51:35 +0000

    Added abstract Pet parent class

--- src/Pet.java
+++ src/Pet.java
@@ -1,3 +1,36 @@
-public class Pet {
+abstract public class Pet {
+    private String name;
+    private int age;
+    private double cost;
 
+    // Gets
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public double getCost(){
+        return this.cost;
+    }
+
+    // Sets
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+
+    public void setCost(double newCost){
+        this.cost = newCost;
+    }
+
+    // Methods
+    public String makeNoise(){
+        return this.name + " does not make a noise!";
+    }
 }
